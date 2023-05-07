7fdfa1cb8b53dd0031b8f37f5de8034ed7054c41
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-05-07 17:16:11 +0200

    rozmrożona dijkstra

--- rozmrozona_dijkstra.py
+++ rozmrozona_dijkstra.py
@@ -1,43 +1,17 @@
-from egzP1btesty import runtests
 from queue import PriorityQueue
 
-def convert_to_adjList(E):
+def rozmnozona_dijkstra(G, start, x):
+    # G - adj. list
+    # x - max path length in terms of edges
 
-    #E: [(node1, node2, weight), ...]
-
-    n = float('-inf')
-
-    for i in range(len(E)):
-
-        if E[i][0] > n:
-            n = E[i][0]
-        if E[i][1] > n:
-            n = E[i][1]
-
-    n += 1
-    G = [[] for _ in range(n)]
-
-    for i in range(len(E)):
-
-        G[E[i][0]].append((E[i][1], E[i][2]))
-        G[E[i][1]].append((E[i][0], E[i][2]))
-
-    return G
-
-def turysta( G, D, L ):
-
-    G = convert_to_adjList(G)
+    x += 1
     n = len(G)
 
-    start = D
-    end = L
-
     n = len(G)
-    visited = [[False for _ in range(5)] for _ in range(n)]
-    D = [[float('inf') for _ in range(5)] for _ in range(n)]
+    visited = [[False for _ in range(x)] for _ in range(n)]
+    D = [[float('inf') for _ in range(x)] for _ in range(n)]
 
-    for i in range(5):
-
+    for i in range(x):
         D[start][i] = 0
 
     Q = PriorityQueue()
@@ -50,11 +24,7 @@ def turysta( G, D, L ):
         curr_node = node[0]
         odw = node[1]
 
-        if curr_node == end and odw == 4:
-            return curr_dist
-
-
-        if not visited[curr_node][odw] and odw < 4:
+        if not visited[curr_node][odw] and odw < (x - 1):
 
             visited[curr_node][odw] = True
 
@@ -68,8 +38,4 @@ def turysta( G, D, L ):
                         D[neighbour][odw + 1] = new_dist
                         Q.put((new_dist, (neighbour, odw + 1)))
 
-
-    return D[end][4]
-
-
-runtests ( turysta )
+    return D
