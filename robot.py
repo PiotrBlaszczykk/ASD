b60d2795aac4c7b57ae1d8be25594ecb7bb7f471
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-08-13 21:09:06 +0200

    robot

--- robot.py
+++ robot.py
@@ -3,24 +3,18 @@ from queue import PriorityQueue
 
 def robot(L, A, B):
 
-    wiersze = len(L)
-    kolumny = len(L[0])
-    Q = PriorityQueue()
-
-    #D: [ x ][ y ][ kierunek  ] = jak szybko sie poruszamy, im szybciej tym lepiej
-    D = [[[-1 for _ in range(4)] for _ in range(kolumny)] for _ in range(wiersze)]
+    #D: [ x ][ y ][ kierunek ] = jak szybko sie poruszamy, im mniej tym lepiej
+    D = [[[61 for _ in range(4)] for _ in range(len(L[0]))] for _ in range(len(L))]
     wynik = [None]
 
     #kierunek: 0 - gora, 1 - prawo, 2 - dol, 3 - lewo
     #time, (x, y, kierunek, speed)
+    Q = PriorityQueue()
     Q.put((0, (A[1], A[0], 1, 60)))
 
     def licz(time, dane):
 
-        x = dane[0]
-        y = dane[1]
-        kierunek = dane[2]
-        speed = dane[3]
+        x, y, kierunek, speed = dane
 
         if L[x][y] == "X":
             return None
@@ -28,20 +22,16 @@ def robot(L, A, B):
             wynik[0] = time
             return None
 
-        fast = None
         if speed == 60:
-            fast = 0
             new_speed = 40
         elif speed == 40:
-            fast = 1
             new_speed = 30
         else:
-            fast = 2
             new_speed = 30
 
-        if D[x][y][kierunek] >= fast:
+        if D[x][y][kierunek] <= speed:
             return None
-        D[x][y][kierunek] = fast
+        D[x][y][kierunek] = speed
 
         if kierunek == 0 or kierunek == 2:
 
@@ -50,7 +40,6 @@ def robot(L, A, B):
 
             if kierunek == 0:
                 Q.put((time + speed, (x + 1, y, 0, new_speed)))
-
             if kierunek == 2:
                 Q.put((time + speed, (x - 1, y, 2, new_speed)))
 
@@ -61,16 +50,13 @@ def robot(L, A, B):
 
             if kierunek == 1:
                 Q.put((time + speed, (x, y + 1, 1, new_speed)))
-
             if kierunek == 3:
                 Q.put((time + speed, (x, y - 1, 3, new_speed)))
-
 
     while not Q.empty():
 
         czas, dane = Q.get()
         licz(czas, dane)
-
         if wynik[0] is not None:
             return wynik[0]
 
