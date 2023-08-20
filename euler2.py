5e136707ed51d698dc7b994e57dd8fa9946cd64e
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-08-20 06:23:54 +0200

    euler dla multigrafu skierowanego

--- euler2.py
+++ euler2.py
@@ -1,33 +1,44 @@
+from queue import PriorityQueue
+
+
 def Euler(G, start):
 
-    #G - adj. list
+	#G - adj. list
+	#multigraf skierowany
 
-    n = len(G)
-    last_index = [0 for _ in range(n)]
-    M = [[0 for _ in range(n)] for _ in range(n)]
+	n = len(G)
+	last_index = [0 for _ in range(n)]
+	M = [[0 for _ in range(n)] for _ in range(n)]
 
-    for i in range(n):
-        for v in G[i]:
-            M[i][v] += 1
+	for i in range(n):
+		for v in G[i]:
+			M[i][v] += 1
 
-    cycle = []
+	cycle = []
+	Q = PriorityQueue()
 
-    def DFSVisit(node):
+	def DFSVisit(depth, node):
 
-        last = last_index[node]
-        # print("wchodze do node", node, "zaczynam od", last)
+		last = last_index[node]
+		# print("wchodze do node", node, "zaczynam od", last)
 
-        for k in range(last, n):
+		for k in range(last, n):
 
-            if M[node][k] > 0:
-                M[node][k] -= 1
-                last = k
-                last_index[node] = last
-                DFSVisit(k)
+			if M[node][k] > 0:
+				M[node][k] -= 1
+				last = k
+				last_index[node] = last
+				Q.put((depth, node))
+				Q.put((depth - 1, k))
+				return None
 
-        last_index[node] = last
-        cycle.append(node)
+		last_index[node] = last
+		cycle.append(node)
 
-    DFSVisit(start)
-    cycle.reverse()
-    return cycle
+	Q.put((0, start))
+	while not Q.empty():
+		depth, node = Q.get()
+		DFSVisit(depth, node)
+
+	cycle.reverse()
+	return cycle
