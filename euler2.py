0331ec07c24b7382b95c7093542679947d2eed6b
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-08-20 13:50:54 +0200

    euler dla multigrafu skierowanego

--- euler2.py
+++ euler2.py
@@ -1,34 +1,30 @@
-from queue import PriorityQueue
-
 def Euler(G, start):
 
     #G - adj. list
     #multigraf skierowany
     n = len(G)
     last_index = [0 for _ in range(n)]
-    M = [[0 for _ in range(n)] for _ in range(n)]
-
-    for i in range(n):
-        for v in G[i]:
-            M[i][v] += 1
-
+    out = [None for _ in range(n)]
     cycle = []
 
+    for i in range(n):
+        out[i] = len(G[i])
+
+    print(G)
     def DFSVisit(node):
 
         last = last_index[node]
-        # print("wchodze do node", node, "zaczynam od", last)
-
-        for k in range(last, n):
-
-            if M[node][k] > 0:
-                M[node][k] -= 1
-                last = k
-                last_index[node] = last
-                DFSVisit(k)
+        #print("wchodze do node", node, "zaczynam od", last)
+        while out[node] != 0:
+            last = last_index[node]
+            v = G[node][last]
+            out[node] -= 1
+            last_index[node] += 1
+            DFSVisit(v)
 
         last_index[node] = last
         cycle.append(node)
+        #print("wychodze z node", node)
 
     DFSVisit(start)
     cycle.reverse()
