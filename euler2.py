790e1c7272521c16ea8235da2d7473242b22cf39
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-08-20 06:08:08 +0200

    Euler 2

---
+++ euler2.py
@@ -0,0 +1,33 @@
+def Euler(G, start):
+
+    #G - adj. list
+
+    n = len(G)
+    last_index = [0 for _ in range(n)]
+    M = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for v in G[i]:
+            M[i][v] += 1
+
+    cycle = []
+
+    def DFSVisit(node):
+
+        last = last_index[node]
+        # print("wchodze do node", node, "zaczynam od", last)
+
+        for k in range(last, n):
+
+            if M[node][k] > 0:
+                M[node][k] -= 1
+                last = k
+                last_index[node] = last
+                DFSVisit(k)
+
+        last_index[node] = last
+        cycle.append(node)
+
+    DFSVisit(start)
+    cycle.reverse()
+    return cycle
