8147175adc7e049fdc83708ecb4de6edf6d2136d
Piotr <132171143+PiotrBlaszczykk@users.noreply.github.com>
2023-08-20 14:19:10 +0200

    Euler

--- euler.py
+++ euler.py
@@ -1,7 +1,9 @@
-def Euler(G, start):
+from collections import deque
 
-    #G - adj. list
-    
+
+def Euler(G, start):
+    # G - adj. list
+
     n = len(G)
     last_index = [0 for _ in range(n)]
     M = [[0 for _ in range(n)] for _ in range(n)]
@@ -30,4 +32,46 @@ def Euler(G, start):
         cycle.append(node)
 
     DFSVisit(start)
+    return cycle
+
+
+def recursion_proof_euler(G, start):
+    # G - adj. list
+    # recursion proof
+    n = len(G)
+    last_index = [0 for _ in range(n)]
+    M = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for v in G[i]:
+            M[i][v] = 1
+
+    cycle = []
+    Q = deque()
+
+    def DFSVisit(node):
+
+        last = last_index[node]
+        # print("wchodze do node", node, "zaczynam od", last)
+
+        for k in range(last, n):
+
+            last = last_index[node]
+            if M[node][k] == 1:
+                M[node][k] = 0
+                M[k][node] = 0
+                last = k + 1
+                last_index[node] = last
+                Q.append(node)
+                Q.append(k)
+                return None
+
+        # print("wychodze z node", node)
+        last_index[node] = last
+        cycle.append(node)
+
+    Q.append(start)
+    while not len(Q) == 0:
+        DFSVisit(Q.pop())
+
     return cycle
